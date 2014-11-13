31afd31aa99eb89a8560ed5db7637dc0255881ed
anson <alexanson520@gmail.com>
2014-11-13 16:05:41 +0800

    my Pro

---
+++ MyPro/MyPro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyPro
+//
+//  Created by Anson on 14/11/13.
+//  Copyright (c) 2014年 Anson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
